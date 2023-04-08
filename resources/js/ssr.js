43f18ee8202cdba24fc751f0a6bfc72e0467ebe6
Vladimir <vladimirborisiuk@gmail.com>
2023-04-08 13:19:17 +0300

    Install Laravel Modules

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -2,14 +2,25 @@ import {createInertiaApp, Link, Head} from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
 import {renderToString} from '@vue/server-renderer';
 import {createSSRApp, h} from 'vue';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createServer(page =>
     createInertiaApp({
         page,
         render: renderToString,
-        resolve: name => {
-            const pages = import.meta.glob('./Pages/**/*.vue')
-            return pages[`./Pages/${name}.vue`]()
+        // resolve: name => {
+        //     const pages = import.meta.glob('./Pages/**/*.vue')
+        //     return pages[`./Pages/${name}.vue`]()
+        // },
+
+        resolve: (name) => {
+            let parts = name.split('::');
+
+            if (parts.length > 1) {
+                return resolvePageComponent(`../../Modules/${parts[0]}/Resources/assets/js/Pages/${parts[1]}.vue`, import.meta.glob(`../../Modules/**/*.vue`));
+            } else {
+                return resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'));
+            }
         },
         setup({App, props, plugin}) {
             return createSSRApp({render: () => h(App, props)})
